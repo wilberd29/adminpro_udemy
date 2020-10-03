c7cac408122547e40a5015e02e24135336306205
Wilber Donayres <wilberd29@gmail.com>
2020-10-02 22:23:08 -0500

    Primer commit udemy

---
+++ src/app/pages/progress/progress.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css']
+})
+export class ProgressComponent implements OnInit {
+
+  progreso:number=50;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+cambiarValor(valor){
+  if(this.progreso>=100 && valor>0){
+    this.progreso=100;
+    return;
+  }
+
+  if(this.progreso<=0 && valor<0){
+    this.progreso=0;
+    return;
+  }
+  this.progreso=this.progreso+valor;
+
+}
+}
